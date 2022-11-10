20293d0acf3100697b5b19009884180a6117a68e
Sean <jordaansean7@gmail.com>
2022-11-10 17:11:17 +0200

    done with projects section

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -2,59 +2,59 @@ export const ProjectData = [
   {
     id: 1,
     title: 'Gym-App',
-    about: 'En Cours',
-    tags: ['ReactJS', 'TypeScript', 'Ghost CMS', 'Html/Css'],
+    about: 'Framework used',
+    tags: ['ReactJS', 'Html/Css', 'Javascript'],
     url: 'https://www.gregoryjaccottet.com',
     image:
-      'https://github.com/ThantZinPhyo-OP/lisandromartinez/raw/main/previews.png',
+      'https://raw.githubusercontent.com/SeanJordaan/Portfolio/main/src/assets/gymapp1.png',
   },
   {
     id: 1,
     title: 'Gym-App',
     about:
-      'This is a ramdom project I am using to React.JS,Javascript, HTML/CSS.This is portfolio website and I hope you are enjoying this portfolio.Make sure you like and comment if you have any doubt',
-    tags: ['ReactJS', 'Javascript', 'Firebase', 'Portfolio'],
-    demo: 'https://github.com/ThantZinPhyo-OP/Portfolio-Website',
-    github: 'https://github.com/ThantZinPhyo-OP/Portfolio-Website',
+      'This is a gym application done useing Reactjs, calling a API for all the workout information. Depolyed using Netlify',
+    tags: ['Netlify'],
+    demo: 'https://seans-gym-app.netlify.app/',
+    github: 'https://github.com/SeanJordaan/Portfolio',
+    image:
+      'https://raw.githubusercontent.com/SeanJordaan/Portfolio/main/src/assets/gymapp2.png',
+  },
+  {
+    id: 2,
+    title: 'Coronavirus-App',
+    about: 'Framework used',
+    tags: ['Java', 'SQL', 'Springboot'],
+    url: 'https://www.gregoryjaccottet.com',
+    image:
+      'https://github.com/ThantZinPhyo-OP/lisandromartinez/raw/main/previews.png',
+  },
+  {
+    id: 2,
+    title: 'Coronavirus-App',
+    about:
+      'A webpage to look at the total number of coronavirus casses and the a total that has been added from the day before the API gets updates daily',
+    tags: ['Heroku'],
+    demo: 'https://aqueous-meadow-74094.herokuapp.com/',
+    github: 'https://github.com/SeanJordaan/gymApp',
     image:
       'https://github.com/ThantZinPhyo-OP/Portfolio-Website/raw/main/home.png',
   },
   {
-    id: 1,
-    title: 'Coronavirus-App',
-    about: 'En Cours',
+    id: 3,
+    title: 'Random Advice',
+    about: 'Framework used',
     tags: ['ReactJS', 'Html/Css'],
     url: 'https://www.gregoryjaccottet.com',
     image:
       'https://github.com/ThantZinPhyo-OP/lisandromartinez/raw/main/previews.png',
   },
   {
-    id: 1,
-    title: 'Coronavirus-App',
-    about:
-      'This is a ramdom project I am using to React.JS,Javascript, HTML/CSS.This is portfolio website and I hope you are enjoying this portfolio.Make sure you like and comment if you have any doubt',
-    tags: ['ReactJS', 'Javascript', 'Firebase', 'Portfolio'],
-    demo: 'https://seans-gym-app.netlify.app/',
-    github: 'https://github.com/SeanJordaan/gymApp',
-    image:
-      'https://github.com/ThantZinPhyo-OP/Portfolio-Website/raw/main/home.png',
-  },
-  {
-    id: 1,
-    title: 'Random Advice',
-    about: 'En Cours',
-    tags: ['ReactJS', 'TypeScript', 'Ghost CMS', 'Html/Css'],
-    url: 'https://www.gregoryjaccottet.com',
-    image:
-      'https://github.com/ThantZinPhyo-OP/lisandromartinez/raw/main/previews.png',
-  },
-  {
-    id: 1,
+    id: 3,
     title: 'Random Advice',
     about:
-      'This is a ramdom project I am using to React.JS,Javascript, HTML/CSS.This is portfolio website and I hope you are enjoying this portfolio.Make sure you like and comment if you have any doubt',
-    tags: ['ReactJS', 'Javascript', 'Firebase', 'Portfolio'],
-    demo: 'https://github.com/ThantZinPhyo-OP/Portfolio-Website',
+      'This webpage can be used on mobel and desktop. It gives you some random advice generated everytime the button is clicked',
+    tags: ['Netlify'],
+    demo: 'https://seans-advice.netlify.app/',
     github: 'https://github.com/ThantZinPhyo-OP/Portfolio-Website',
     image:
       'https://github.com/ThantZinPhyo-OP/Portfolio-Website/raw/main/home.png',
